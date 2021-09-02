6887f8c2bdb0d7f94b0ee95a9839388a6e0cfd30
Zoran Đukić <zorandjukic.os@gmail.com>
2021-09-02 21:32:09 +0200

    Spring vjezba

--- sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -1,6 +1,9 @@
 package guru.springframework.sfgdi;
 
+import guru.springframework.sfgdi.config.SfgConfiguration;
+import guru.springframework.sfgdi.config.SfgConstructorConfig;
 import guru.springframework.sfgdi.controllers.*;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.services.PrototypeBean;
 import guru.springframework.sfgdi.services.SingletonBean;
 import org.springframework.boot.SpringApplication;
@@ -8,54 +11,70 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"guru.springframework.sfgdi","com.springframework.pets;"})
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "com.springframework.pets;"})
 @SpringBootApplication
 public class SfgDiApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		//https://stackoverflow.com/questions/19615972/application-context-what-is-this
-		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+        //https://stackoverflow.com/questions/19615972/application-context-what-is-this
+        ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
 
-		PetController petController = ctx.getBean("petController", PetController.class);
-		System.out.println("----The best pet is----");
-		System.out.println(petController.whichPetIsTheBest());
+        PetController petController = ctx.getBean("petController", PetController.class);
+        System.out.println("----The best pet is----");
+        System.out.println(petController.whichPetIsTheBest());
 
-		I18nContoller i18nContoller = (I18nContoller) ctx.getBean("i18nContoller");
-		System.out.println(i18nContoller.sayHello());
+        I18nContoller i18nContoller = (I18nContoller) ctx.getBean("i18nContoller");
+        System.out.println(i18nContoller.sayHello());
 
-		MyController myController = (MyController) ctx.getBean("myController");
-
-
-		System.out.println("-----Primary bean");
-		System.out.println(myController.sayHello());
+        MyController myController = (MyController) ctx.getBean("myController");
 
 
-		System.out.println("------ Property");
-		PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-		System.out.println(propertyInjectedController.getGreeting());
-
-		System.out.println("-----Setter");
-		SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-		System.out.println(setterInjectedController.getGreeting());
-
-		System.out.println("-----Constructor");
-		ConstuctorInjectedController constuctorInjectedController=(ConstuctorInjectedController) ctx.getBean("constuctorInjectedController");
-		System.out.println(constuctorInjectedController.getGreeting());
-
-		System.out.println("-----Bean Scopes -------");
-		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
-		System.out.println(singletonBean1.getMyScope());
-		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
-		System.out.println(singletonBean2.getMyScope());
-
-		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
-		System.out.println(prototypeBean1.getMyScope());
-		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
-		System.out.println(prototypeBean2.getMyScope());
+        System.out.println("-----Primary bean");
+        System.out.println(myController.sayHello());
 
 
-	}
+        System.out.println("------ Property");
+        PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
+        System.out.println(propertyInjectedController.getGreeting());
+
+        System.out.println("-----Setter");
+        SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
+        System.out.println(setterInjectedController.getGreeting());
+
+        System.out.println("-----Constructor");
+        ConstuctorInjectedController constuctorInjectedController = (ConstuctorInjectedController) ctx.getBean("constuctorInjectedController");
+        System.out.println(constuctorInjectedController.getGreeting());
+
+        System.out.println("-----Bean Scopes -------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
+
+        FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
+        System.out.println(fakeDataSource.getUsername());
+        System.out.println(fakeDataSource.getPassword());
+        System.out.println(fakeDataSource.getJdbcurl());
+
+//        System.out.println("----------Config props bean");
+//        SfgConfiguration sfgConfiguration = ctx.getBean(SfgConfiguration.class
+//        );
+//        System.out.println(sfgConfiguration.getUsername());
+//        System.out.println(sfgConfiguration.getPassword());
+//        System.out.println(sfgConfiguration.getJdbcurl());
+
+        System.out.println("----------- Constructor Binding");
+        SfgConstructorConfig sfgConstructorConfig = ctx.getBean(SfgConstructorConfig.class);
+        System.out.println(sfgConstructorConfig.getUsername());
+        System.out.println(sfgConstructorConfig.getPassword());
+        System.out.println(sfgConstructorConfig.getJdbcurl());
+    }
 
 
 }
--- sfg-di/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ sfg-di/src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,72 +2,84 @@ package guru.springframework.sfgdi.config;
 
 import com.springframework.pets.PetService;
 import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.datasource.FakeDataSource;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
 import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.*;
 import org.springframework.stereotype.Service;
 
+@EnableConfigurationProperties(SfgConstructorConfig.class)
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    PetServiceFactory petServiceFactory(){
-        return new PetServiceFactory();
-    }
+    FakeDataSource fakeDataSource(@Value("${guru.username}")String username,
+                                  @Value("${guru.password}")String password,
+                                  @Value("${guru.jdbcurl}")String jdbcurl) {
+        FakeDataSource fakeDataSource = new FakeDataSource();
+        fakeDataSource.setUsername(username);
+        fakeDataSource.setPassword(password);
+        fakeDataSource.setJdbcurl(jdbcurl);
+            return fakeDataSource;
+        }
 
-    @Profile({"dog","default"})
-    @Bean
-    PetService dogPetService(PetServiceFactory petServiceFactory){
-        return petServiceFactory().getPetService("dog");
-    }
-    @Profile({"cat"})
-    @Bean
-    PetService catPetService(PetServiceFactory petServiceFactory){
-        return petServiceFactory.getPetService("cat");
-    }
+        @Bean
+        PetServiceFactory petServiceFactory () {
+            return new PetServiceFactory();
+        }
 
-    @Bean
-    EnglishGreetingRepository englishGreetingRepository(){
-        return new EnglishGreetingRepositoryImpl();
-    }
+        @Profile({"dog", "default"})
+        @Bean
+        PetService dogPetService (PetServiceFactory petServiceFactory){
+            return petServiceFactory().getPetService("dog");
+        }
+        @Profile({"cat"})
+        @Bean
+        PetService catPetService (PetServiceFactory petServiceFactory){
+            return petServiceFactory.getPetService("cat");
+        }
 
-    @Profile("EN")
-    @Bean
-    I18nEnglishGreetingService i18Service(EnglishGreetingRepository englishGreetingRepository) {
-        return new I18nEnglishGreetingService(englishGreetingRepository);
-    }
+        @Bean
+        EnglishGreetingRepository englishGreetingRepository () {
+            return new EnglishGreetingRepositoryImpl();
+        }
 
-    @Profile("ES")
-    @Bean("i18nService")
-    I18SpanishGreetingService i18nService() {
-        return new I18SpanishGreetingService();
-    }
+        @Profile("EN")
+        @Bean
+        I18nEnglishGreetingService i18Service (EnglishGreetingRepository englishGreetingRepository){
+            return new I18nEnglishGreetingService(englishGreetingRepository);
+        }
 
-
-    @Primary
-    @Bean
-    PrimaryGreetingService primaryGreetingService() {
-        return new PrimaryGreetingService();
-    }
+        @Profile("ES")
+        @Bean("i18nService")
+        I18SpanishGreetingService i18nService () {
+            return new I18SpanishGreetingService();
+        }
 
 
-    @Bean
-    ConstructorGreetingService constructorGreetingService() {
-        return new ConstructorGreetingService();
-    }
+        @Primary
+        @Bean
+        PrimaryGreetingService primaryGreetingService () {
+            return new PrimaryGreetingService();
+        }
 
-    @Bean
-    PropertyInjectedGreetingService propertyInjectedGreetingService() {
-        return new PropertyInjectedGreetingService();
-    }
 
-    @Bean
-    SetterInjectedGreetingService setterInjectedGreetingService() {
-        return new SetterInjectedGreetingService();
+        @Bean
+        ConstructorGreetingService constructorGreetingService () {
+            return new ConstructorGreetingService();
+        }
+
+        @Bean
+        PropertyInjectedGreetingService propertyInjectedGreetingService () {
+            return new PropertyInjectedGreetingService();
+        }
+
+        @Bean
+        SetterInjectedGreetingService setterInjectedGreetingService () {
+            return new SetterInjectedGreetingService();
+        }
     }
-}
